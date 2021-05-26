b174367676386c80e05605825ed5ab05a1f3e901
YanHui-Yang <124481952@qq.com>
2021-05-26 16:35:10 +0800

    修改功能

---
+++ LinkedList/src/main/java/com/yyh/Test.java
@@ -0,0 +1,20 @@
+package com.yyh;
+
+/**
+ * Author: Tong
+ * Date: 2021/5/26
+ * Description: 测试链表
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        DummyHeadLinkedList<Integer> linkedList = new DummyHeadLinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(2, 666);
+        System.out.println(linkedList);
+    }
+}
