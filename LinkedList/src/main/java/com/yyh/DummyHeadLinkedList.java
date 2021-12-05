053e46f0976912e90fe3e0aaa9e372e1685f89ba
YanHui-Yang <124481952@qq.com>
2021-12-05 16:32:01 +0800

    feat: 使用dummyHead作为头节点

---
+++ LinkedList/src/main/java/com/yyh/DummyHeadLinkedList.java
@@ -0,0 +1,66 @@
+package com.yyh;
+
+public class DummyHeadLinkedList<E> {
+    private class Node<E> {
+        private E e;
+        private Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private int size;
+    private Node dummyHead;
+
+    public DummyHeadLinkedList() {
+        this.dummyHead = null;
+        this.size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 向链表指定索引处添加元素
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Require index >= 0 and index <= size.");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    // 链表头插入一个元素
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    // 链表尾插入一个元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+}
