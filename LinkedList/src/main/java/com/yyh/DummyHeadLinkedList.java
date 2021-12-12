71b6bbe8cc483f12672be0e33d794bdaa2a0d99e
YanHui-Yang <124481952@qq.com>
2021-12-12 22:13:15 +0800

    feat: 为链表添加虚拟头节点

--- LinkedList/src/main/java/com/yyh/DummyHeadLinkedList.java
+++ LinkedList/src/main/java/com/yyh/DummyHeadLinkedList.java
@@ -148,17 +148,6 @@ public class DummyHeadLinkedList<E> {
         return sb.toString();
     }
 
-    /*public static void main(String[] args) {
-        DummyHeadLinkedList<Integer> linkedList = new DummyHeadLinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            linkedList.addFirst(i);
-            System.out.println(linkedList);
-        }
-
-        linkedList.add(2, 666);
-        System.out.println(linkedList);
-    }*/
-
     public static void main(String[] args) {
         DummyHeadLinkedList<Integer> linkedList = new DummyHeadLinkedList<>();
         for(int i = 0 ; i < 5 ; i ++){
