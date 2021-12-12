cebfb4491de8d7cfe30acf1e6877221c26241f21
YanHui-Yang <124481952@qq.com>
2021-12-12 21:17:39 +0800

    feat: 使用链表实现栈

---
+++ LinkedList/src/main/java/com/yyh/StackLinkedList.java
@@ -0,0 +1,56 @@
+package com.yyh;
+
+public class StackLinkedList<E> implements Stack<E> {
+
+    private DummyHeadLinkedList<E> list;
+
+    public StackLinkedList() {
+        list = new DummyHeadLinkedList<>();
+    }
+
+    @Override
+    public int getSize() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+    @Override
+    public E peek() {
+        return list.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Stack: top ");
+        res.append(list);
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        StackLinkedList<Integer> stack = new StackLinkedList<>();
+
+        for(int i = 0 ; i < 5 ; i ++){
+            stack.push(i);
+            System.out.println(stack);
+        }
+
+        stack.pop();
+        System.out.println(stack);
+    }
+}
